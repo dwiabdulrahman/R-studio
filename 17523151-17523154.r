c65c020518dd906a2bc336b1a122ac0ca3fafd5b
dwiabdulrahman <43625554+dwiabdulrahman@users.noreply.github.com>
2018-12-16 12:30:57 +0700

    Add files via upload

---
+++ 17523151-17523154.r
@@ -0,0 +1,15 @@
+#NAMA :
+  #-Girendra Egie Zuhrival (17523154)
+  #-DWI ABDUL RAHMAN ( 17523151 )
+
+
+#----------------------------------
+#nomer 1b
+library(matlib)
+A = matrix(c(4,-5,2,-3),2,2,TRUE)
+powerMethod(A, v = c(1, 0),maxiter = 5)
+#----------------------------------
+#nomer 2b
+library(matlib)
+A = matrix(c(0,11,-5,-2,17,-7,-4,26,-10),3,3,TRUE)
+powerMethod(A, v = c(1, 1,1),maxiter = 5)
